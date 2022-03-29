a04260f730e35b132ee26dbf516fbe8dbce6a762
Okan Cetin <okan.cetin@episerver.com>
2022-03-29 03:08:55 +0200

    Fix stale sort selection bug

--- client/components/content/LocationListPage/LocationList.Sort.tsx
+++ client/components/content/LocationListPage/LocationList.Sort.tsx
@@ -6,7 +6,7 @@ import {
   MenuOptionGroup,
   MenuItemOption,
 } from "@chakra-ui/react"
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { CgSortAz } from "react-icons/cg"
 
 import { LocationListContext } from "./LocationList.Context"
@@ -18,15 +18,21 @@ const sortMap: Record<string, Partial<LocationSort>> = {
 }
 
 export const LocationListSort: React.FC = () => {
+  const [orderBy, setOrderBy] = useState("")
   const {
     filters: [filters, setFilters],
   } = useContext(LocationListContext)
 
   const handleSortSelection = (option: any) => {
-    if (option) {
-      setFilters({ orderBy: sortMap[option] })
-    }
+    setOrderBy(option)
+    setFilters({ ...filters, orderBy: sortMap[option] })
   }
+
+  useEffect(() => {
+    if (!filters.orderBy) {
+      setOrderBy("")
+    }
+  }, [filters])
 
   return (
     <Menu closeOnSelect={true}>
@@ -39,7 +45,7 @@ export const LocationListSort: React.FC = () => {
         Sorting
       </MenuButton>
       <MenuList zIndex={999}>
-        <MenuOptionGroup type="radio" onChange={handleSortSelection}>
+        <MenuOptionGroup value={orderBy} type="radio" onChange={handleSortSelection}>
           <MenuItemOption value="name">Name</MenuItemOption>
           <MenuItemOption value="highTemp">Highest temperature</MenuItemOption>
           <MenuItemOption value="lowTemp">Lowest temperature</MenuItemOption>
